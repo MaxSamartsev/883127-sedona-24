2fc432949f664bfd270e9be204655ef26067870e
MaxSamartsev <45741458+MaxSamartsev@users.noreply.github.com>
2019-03-02 16:55:26 +0300

    Выполнил доработки согласно критериям

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ var searchForm =  document.querySelector(".sentence-form");
 var dateIn = searchForm.querySelector("[name=arrival-date]");
 
 if (searchOpen) {
-  if (searchForm) {
-    searchForm.classList.add("sentence-form-hide");
+    if (searchForm) {
+      searchForm.classList.add("sentence-form-hide");
+    };
+  
+    searchOpen.addEventListener("click", function (event) {
+      event.preventDefault();
+      searchForm.classList.toggle("sentence-form-show");
+      searchForm.classList.toggle("sentence-form-hide");
+      dateIn.focus();
+    });
   };
-
-  searchOpen.addEventListener("click", function (event) {
-    event.preventDefault();
-    searchForm.classList.toggle("sentence-form-hide");
-    searchForm.classList.toggle("sentence-form-show");
-    dateIn.focus();
-  });
-};
